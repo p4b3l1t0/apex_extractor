a9ba61c24341cc2214b5ad8814840ee60f505a7e
p4b3l1t0 <pablosalinas1717@gmail.com>
2023-12-10 18:29:08 -0500

    first commit

---
+++ extract_apex.py
@@ -0,0 +1,23 @@
+import re
+
+def extract_controllers(file_path):
+    # Regular expression pattern to match aura:// and apex:// patterns
+    pattern = r'(aura://[\w.\/$]+|apex://[\w.\/$]+)'
+
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            content = file.read()
+
+        # Find all matches
+        matches = re.findall(pattern, content)
+
+        return matches
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return []
+
+# Usage example
+file_path = 'app.js'  # Replace with the path to your app.js file
+controllers = extract_controllers(file_path)
+for controller in controllers:
+    print(controller)
